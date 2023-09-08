aae989a02fdaa321761943d4926fa948c435ff42
Aryan <as2280@srmist.edu.in>
2023-09-08 15:58:34 +0530

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const app = express();
-
+const port=process.env.PORT || 5000;
 app.use(express.json());
 app.use(bodyParser.json());
 app.get('/',(req,res)=>{
@@ -55,6 +56,6 @@ app.use((req, res) => {
     res.status(404).send("404 URL NOT FOUND");
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("listening on port 5000");
   });
